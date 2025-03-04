ae07382e0a6fbfb696545b83825fc7c17b8f2e33
ricknullsec <177259409+ricknullsec@users.noreply.github.com>
2025-03-03 20:38:01 -0800

    Implemented selling and collecting

--- eco-game.py
+++ eco-game.py
@@ -26,13 +26,38 @@ class Corporation:
     
     def buildMine(self, name):
         self.mines.append(Mine(name,self.name))
+        self.money -= 1000
+    
+    def listMines(self):
+        for mine in self.mines:
+            print(mine.name)
+    
+    def collectAllMinesInventory(self):
+        for mine in self.mines:
+            self.storage += mine.storage
+            mine.storage -= mine.storage
+    
+    def sellInventory(self):
+        self.money += self.storage
+        print(self.name + " has sold " + str(self.storage) + " units.")
+        self.storage -= self.storage
+        print("Its new balance is $" + str(self.money))
     
 
     
 playerCorp = Corporation("Black Inc")
 playerCorp.buildMine("Gold Mine")
 
+for day in range(1,11):
+    for mine in playerCorp.mines:
+        mine.produce()
 
-for mine in playerCorp.mines:
-    mine.produce()
+playerCorp.collectAllMinesInventory()
+playerCorp.sellInventory()
+playerCorp.buildMine("Silver Mine")
 
+for day in range(1,11):
+    for mine in playerCorp.mines:
+        mine.produce()
+playerCorp.collectAllMinesInventory()
+playerCorp.sellInventory()
